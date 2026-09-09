24b95bc8510e3acad3927f78d8bd5bd56b8b0368
bia-fialho <bia.fialho@gmail.com>
2026-09-09 09:56:21 -0300

    segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -66,6 +66,33 @@ Widget build(BuildContext context) {
   );
 }
 }
+
+class AppInfo{
+  final String nome;
+  final String descricao;
+  final IconData icone;
+
+  const AppInfo({
+    required this.nome,
+    required this.descricao,
+    required this.icone,
+  });
+
+}
+
+final List<AppInfo> meusApps=[
+ AppInfo(
+  nome: 'Calculadora de Gasolina',
+  descricao: 'Calcula litros e custo de uma viagem',
+  icone: Icons.local_gas_station,
+ ),
+ AppInfo(
+  nome:'Calculadora de Churrasco',
+  descricao: 'Calcula carne, bebida e carvão',
+  icone: Icons.outdoor_grill, 
+ )
+];
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
@@ -75,9 +102,85 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("LupTok"),
       ),
-      body: const Center(
-        child: Text('Em construção ...'),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: const Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  'LupTok',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              )
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text ('Início'),
+              onTap: () => Navigator.pop(context),
+            ),
+             ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text ('Perfil'),
+              onTap: () => Navigator.pop(context),
+            ),
+             ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text ('Sair'),
+              onTap: () => Navigator.pop(context),
+            ),
+          ],
+        )
       ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(16),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisExtent: 12,
+          crossAxisSpacing: 12,
+          childAspectRatio: 0.95, 
+          ),
+          itemCount: meusApps.length,
+          itemBuilder: (context, indice){
+            final app = meusApps[indice];
+            return Card(
+              child: Padding(
+                padding: EdgeInsets.all(12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      app.icone,
+                      size: 36,
+                      color: Theme.of(context).colorScheme.primary
+                    ),
+                    SizedBox(height: 8,),
+                    Text(
+                      app.nome,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                      )
+                    ),
+                    SizedBox(height: 4,),
+                    Text(
+                      app.descricao,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 11,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }
+      )
     );
   }
 }
